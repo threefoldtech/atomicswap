8188a684b737b8d95ff370d687c9cba4bcff9af9
Rob Van Mieghem <robvanmieghem@gmail.com>
2019-10-03 20:19:00 +0200

    Add initial CreateHoldingAccount function

--- cmd/stellaratomicswap/stellar/stellar.go
+++ cmd/stellaratomicswap/stellar/stellar.go
@@ -4,11 +4,89 @@ package stellar
 
 import (
 	"github.com/stellar/go/keypair"
+	"github.com/stellar/go/strkey"
+	"github.com/stellar/go/txnbuild"
+
+	"github.com/stellar/go/protocols/horizon"
 )
 
 //GenerateKeyPair creates a new stellar full keypair
 func GenerateKeyPair() (pair *keypair.Full, err error) {
 
 	pair, err = keypair.Random()
+	return
+}
+
+func createHashxAddress(hash []byte) (address string, err error) {
+	return strkey.Encode(strkey.VersionByteHashX, hash)
+}
+
+func createHashTxAddress(hash []byte) (address string, err error) {
+	return strkey.Encode(strkey.VersionByteHashTx, hash)
+}
+
+//CreateHoldingAccount creates a new account to hold the atomic swap balance
+//with the signers modified to the atomic swap rules:
+//- signature of the destinee and the secret
+//- hash of a specific transaction that is present on the chain
+//    that merges the escrow account to the account that needs to withdraw
+//    and that can only be published in the future ( timeout mechanism)
+func CreateHoldingAccount(xlmAmount string, withdrawalAccount horizon.Account, counterPartyAddress string, secretHash []byte, refundTxHash []byte, network string) (createAccountTransaction txnbuild.Transaction,holdingAccountAddress string, err error) {
+	holdingKeyPair, err := GenerateKeyPair()
+	if err != nil {
+		return
+	}
+	holdingAccountAddress = holdingKeyPair.Address()
+	accountCreationOperation := txnbuild.CreateAccount{
+		Destination:   holdingAccountAddress,
+		Amount:        xlmAmount,
+		SourceAccount: &withdrawalAccount,
+	}
+
+	depositorSigningOperation := txnbuild.SetOptions{
+		Signer: &txnbuild.Signer{
+			Address: counterPartyAddress,
+			Weight:  1,
+		},
+	}
+	secretHashAddress, err := createHashxAddress(secretHash)
+	if err != nil {
+		return
+	}
+	secretSigningOperation := txnbuild.SetOptions{
+		Signer: &txnbuild.Signer{
+			Address: secretHashAddress,
+			Weight:  1,
+		},
+	}
+	refundTxHashAdddress, err := createHashTxAddress(refundTxHash)
+	if err != nil {
+		return
+	}
+	refundSigningOperation := txnbuild.SetOptions{
+		Signer: &txnbuild.Signer{
+			Address: refundTxHashAdddress,
+			Weight:  2,
+		},
+	}
+	setSigingWeightsOperation := txnbuild.SetOptions{
+		MasterWeight:    txnbuild.NewThreshold(txnbuild.Threshold(uint8(0))),
+		LowThreshold:    txnbuild.NewThreshold(txnbuild.Threshold(2)),
+		MediumThreshold: txnbuild.NewThreshold(txnbuild.Threshold(2)),
+		HighThreshold:   txnbuild.NewThreshold(txnbuild.Threshold(2)),
+	}
+	createAccountTransaction=txnbuild.Transaction{
+		SourceAccount:withdrawalAccount,
+		Operations:[]txnbuild.Operation{
+			&accountCreationOperation,
+			depositorSigningOperation,
+			secretSigningOperation,
+			refundSigningOperation,
+			setSigingWeightsOperation,
+		},
+		Network:network,
+		Timebounds:txnbuild.NewInfiniteTimeout()//TODO: Use a real timeout 
+	}
+
 	return
 }
