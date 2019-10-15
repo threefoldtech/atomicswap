7cff45ee573e1281facc7ce08db39f6541b61259
Rob Van Mieghem <robvanmieghem@gmail.com>
2019-10-15 14:46:57 +0200

    Stellar audit command

--- cmd/stellaratomicswap/main.go
+++ cmd/stellaratomicswap/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"crypto/rand"
 	"crypto/sha256"
 	"encoding/hex"
@@ -9,13 +10,17 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"reflect"
 	"strconv"
 	"strings"
+	"time"
 
-	"github.com/stellar/go/txnbuild"
+	"github.com/stellar/go/strkey"
 
 	"github.com/stellar/go/clients/horizon"
 	"github.com/stellar/go/clients/horizonclient"
+	hprotocol "github.com/stellar/go/protocols/horizon"
+	"github.com/stellar/go/txnbuild"
 	"github.com/threefoldtech/atomicswap/cmd/stellaratomicswap/stellar"
 
 	"github.com/stellar/go/keypair"
@@ -67,7 +72,7 @@ func init() {
 		fmt.Println("  redeem <contract> <contract transaction> <secret>")
 		fmt.Println("  refund <contract> <contract transaction>")
 		fmt.Println("  extractsecret <redemption transaction> <secret hash>")
-		fmt.Println("  auditcontract <holdingAccountAdress>")
+		fmt.Println("  auditcontract <holdingAccountAdress> < refund transaction>")
 		fmt.Println()
 		fmt.Println("Flags:")
 		flagset.PrintDefaults()
@@ -114,7 +119,7 @@ type extractSecretCmd struct {
 }
 
 type auditContractCmd struct {
-	contract             []byte
+	refundTx             txnbuild.Transaction
 	holdingAccountAdress string
 }
 
@@ -243,7 +248,11 @@ func run() (showUsage bool, err error) {
 		if err != nil {
 			return true, fmt.Errorf("invalid holding account address: %v", err)
 		}
-		cmd = &auditContractCmd{holdingAccountAdress: args[1]}
+		refundTransaction, err := txnbuild.TransactionFromXDR(args[2])
+		if err != nil {
+			return true, fmt.Errorf("failed to decode refund transaction: %v", err)
+		}
+		cmd = &auditContractCmd{holdingAccountAdress: args[1], refundTx: refundTransaction}
 
 	}
 	err = cmd.runCommand(client)
@@ -517,5 +526,133 @@ func (cmd *participateCmd) runCommand(client horizonclient.ClientInterface) erro
 }
 
 func (cmd *auditContractCmd) runCommand(client horizonclient.ClientInterface) error {
+	holdingAccount, err := client.AccountDetail(horizonclient.AccountRequest{AccountID: cmd.holdingAccountAdress})
+	if err != nil {
+		return err
+	}
+	balance, err := holdingAccount.GetNativeBalance() //TODO: modify for other assets
+	if err != nil {
+		return err
+	}
+	//Check if the signing tresholds are correct
+	if holdingAccount.Thresholds.HighThreshold != 2 || holdingAccount.Thresholds.MedThreshold != 2 || holdingAccount.Thresholds.LowThreshold != 2 {
+		return fmt.Errorf("Holding account signing tresholds are wrong.\nTresholds: High: %d, Medium: %d, Low: %d", holdingAccount.Thresholds.HighThreshold, holdingAccount.Thresholds.MedThreshold, holdingAccount.Thresholds.LowThreshold)
+	}
+	//Get the signing conditions
+	var refundTxHashFromSigningConditions []byte
+	recipientAddress := ""
+	var secretHash []byte
+	for _, signer := range holdingAccount.Signers {
+		if signer.Weight == 0 { //The original keypair's signing weight is set to 0
+			continue
+		}
+		switch signer.Type {
+		case hprotocol.KeyTypeNames[strkey.VersionByteAccountID]:
+			if recipientAddress != "" {
+				return fmt.Errorf("Multiple recipients as signer: %s and %s", recipientAddress, signer.Key)
+			}
+			recipientAddress = signer.Key
+			if signer.Weight != 1 {
+				return fmt.Errorf("Signing weight of the recipient is wrong. Recipient: %s Weight: %d", signer.Key, signer.Weight)
+			}
+		case hprotocol.KeyTypeNames[strkey.VersionByteHashTx]:
+			if refundTxHashFromSigningConditions != nil {
+				return errors.New("Multiple refund transaction hashes as signer")
+			}
+
+			refundTxHashFromSigningConditions, err = strkey.Decode(strkey.VersionByteHashTx, signer.Key)
+			if err != nil {
+				return fmt.Errorf("Faulty encoded refund transaction hash: %s", err)
+			}
+			if signer.Weight != 2 {
+				return fmt.Errorf("Signing weight of the refund transaction is wrong. Weight: %d", signer.Weight)
+			}
+
+		case hprotocol.KeyTypeNames[strkey.VersionByteHashX]:
+			if secretHash != nil {
+				return fmt.Errorf("Multiple secret hashes  transaction hashes as signer: %s and %s", secretHash, signer.Key)
+			}
+			secretHash, err = strkey.Decode(strkey.VersionByteHashX, signer.Key)
+			if err != nil {
+				return fmt.Errorf("Faulty encoded secret hash: %s", err)
+			}
+			if signer.Weight != 1 {
+				return fmt.Errorf("Signing weight of the secret hash is wrong. Weight: %d", signer.Weight)
+			}
+		default:
+			return fmt.Errorf("Unexpected signer type: %s", signer.Type)
+		}
+	}
+	//Make sure all signing conditions are present
+	if refundTxHashFromSigningConditions == nil {
+		return errors.New("Missing refund transaction hash as signer")
+	}
+	if secretHash == nil {
+		return errors.New("Missing secret as signer")
+	}
+	if recipientAddress == "" {
+		return errors.New("Missing recipient as signer")
+	}
+	//Compare the refund transaction hash in the signing condition to the one of the passed refund transaction
+	cmd.refundTx.Network = targetNetwork
+	refundTxHash, err := cmd.refundTx.Hash()
+	if err != nil {
+		return fmt.Errorf("Unable to hash the passed refund transaction: %v", err)
+	}
+	if !bytes.Equal(refundTxHashFromSigningConditions, refundTxHash[:]) {
+		return errors.New("Refund transaction hash in the signing condition is not equal to the one of the passed refund transaction")
+	}
+	//and finally get the locktime and refund address
+	lockTime := cmd.refundTx.Timebounds.MinTime
+	if len(cmd.refundTx.Operations) != 1 {
+		return fmt.Errorf("Refund transaction is expected to have 1 operation instead of %d", len(cmd.refundTx.Operations))
+	}
+	refundoperation := cmd.refundTx.Operations[0]
+	accountMergeOperation, ok := cmd.refundTx.Operations[0].(*txnbuild.AccountMerge)
+	if !ok {
+		return fmt.Errorf("Expecting an accountmerge operation in the refund transaction but got a %v", reflect.TypeOf(refundoperation))
+	}
+	if accountMergeOperation.SourceAccount.GetAccountID() != cmd.holdingAccountAdress {
+		return fmt.Errorf("The refund transaction does not refund from the holding account but from %v", accountMergeOperation.SourceAccount.GetAccountID())
+
+	}
+	refundAddress := accountMergeOperation.Destination
+	if !*automatedFlag {
+		fmt.Printf("Contract address:        %v\n", cmd.holdingAccountAdress)
+		fmt.Printf("Contract value:          %v\n", balance)
+		fmt.Printf("Recipient address:       %v\n", recipientAddress)
+		fmt.Printf("Author's refund address: %v\n\n", refundAddress)
+
+		fmt.Printf("Secret hash: %x\n\n", secretHash)
+
+		t := time.Unix(lockTime, 0)
+		fmt.Printf("Locktime: %v\n", t.UTC())
+		reachedAt := time.Until(t).Truncate(time.Second)
+		if reachedAt > 0 {
+			fmt.Printf("Locktime reached in %v\n", reachedAt)
+		} else {
+			fmt.Printf("Refund time lock has expired\n")
+		}
+	} else {
+		output := struct {
+			ContractAddress  string `json:"contractAddress"`
+			ContractValue    string `json:"contractValue"`
+			RecipientAddress string `json:"recipientAddress"`
+			RefundAddress    string `json:"refundAddress"`
+			SecretHash       string `json:"secretHash"`
+			Locktime         string `json:"Locktime"`
+		}{
+			fmt.Sprintf("%v", cmd.holdingAccountAdress),
+			fmt.Sprintf("%v", balance),
+			recipientAddress,
+			refundAddress,
+			fmt.Sprintf("%x", secretHash),
+			"",
+		}
+		t := time.Unix(lockTime, 0)
+		output.Locktime = fmt.Sprintf("%v", t.UTC())
+		jsonoutput, _ := json.Marshal(output)
+		fmt.Println(string(jsonoutput))
+	}
 	return nil
 }
