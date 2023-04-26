63a9c8c2aeacc2f113281454f033ef024fa700b6
Lee Smet <lee.smet@hotmail.com>
2023-04-26 17:36:55 +0200

    Isoalte iethereum Initiate command
    Signed-off-by: Lee Smet <lee.smet@hotmail.com>

---
+++ eth/initiate.go
@@ -0,0 +1,50 @@
+package eth
+
+import (
+	"context"
+	"crypto/rand"
+	"crypto/sha256"
+	"fmt"
+	"math/big"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/rlp"
+)
+
+type InitiateOutput struct {
+	Secret           [32]byte       `json:"secret"`
+	SecretHash       [32]byte       `json:"secret_hash"`
+	InitiatorAddress common.Address `json:"initiator_address"`
+}
+
+func Initiate(ctx context.Context, sct swapContractTransactor, cp2Addr common.Address, amount *big.Int) (InitiateOutput, error) {
+	secret, secretHash := generateSecretHashPair()
+	tx, err := sct.initiateTx(ctx, amount, secretHash, cp2Addr)
+	if err != nil {
+		return InitiateOutput{}, fmt.Errorf("failed to create initiate TX: %v", err)
+	}
+
+	txBytes, err := rlp.EncodeToBytes(tx)
+	if err != nil {
+		return InitiateOutput{}, fmt.Errorf("failed to encode contract TX: %v", err)
+	}
+	fmt.Printf("%x\n\n", txBytes)
+
+	err = tx.Send(ctx)
+	if err != nil {
+		return InitiateOutput{}, err
+	}
+	fmt.Printf("Published contract transaction (%x)\n", tx.Hash())
+
+	return InitiateOutput{
+		Secret:           secret,
+		SecretHash:       secretHash,
+		InitiatorAddress: sct.fromAddr,
+	}, nil
+}
+
+func generateSecretHashPair() (secret, secretHash [sha256.Size]byte) {
+	rand.Read(secret[:])
+	secretHash = sha256Hash(secret[:])
+	return
+}
