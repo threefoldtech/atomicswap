59aee7e477f0e648643c505e631287b6a484f7ce
Lee Smet <lee.smet@hotmail.com>
2023-04-26 18:20:05 +0200

    Isolate ethereum refund command
    Signed-off-by: Lee Smet <lee.smet@hotmail.com>

---
+++ eth/refund.go
@@ -0,0 +1,27 @@
+package eth
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+)
+
+func Refund(ctx context.Context, sct swapContractTransactor, contractTx *types.Transaction) (common.Hash, error) {
+	params, err := unpackContractInputParams(sct.abi, contractTx)
+	if err != nil {
+		return common.Hash{}, err
+	}
+	tx, err := sct.refundTx(ctx, params.SecretHash)
+	if err != nil {
+		return common.Hash{}, fmt.Errorf("failed to create refund TX: %v", err)
+	}
+
+	err = tx.Send(ctx)
+	if err != nil {
+		return common.Hash{}, err
+	}
+	return tx.Hash(), nil
+
+}
